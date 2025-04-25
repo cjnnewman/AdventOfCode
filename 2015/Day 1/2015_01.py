70e7faa79a4932e1d3dd2c5f0fd511f9392f7ceb
Caleb Newman <newmanc3gen@gmail.com>
2025-04-25 19:36:43 -0400

    Completed day 1

--- 2015/Day 1/2015_01.py
+++ 2015/Day 1/2015_01.py
@@ -4,19 +4,19 @@ def santa_directions():
     
     floor = 0
     current_char = 0
+    basement_first = False
+
     for char in char_list:
         current_char += 1
         if char == '(':
             floor += 1
-            if floor == -1:
-                print(f"Entered basement on character number: {current_char}")
-                break
         elif char == ')':
             floor -= 1
-            if floor == -1:
-                print(f"Entered basement on character number: {current_char}")
-                break
-            
+            if floor == -1 and basement_first == False:
+                basement_char = current_char
+                basement_first = True
+
+    print(f"First character to enter the basement: {basement_char}")        
     print(f"Final floor: {floor}")
 
 santa_directions()
